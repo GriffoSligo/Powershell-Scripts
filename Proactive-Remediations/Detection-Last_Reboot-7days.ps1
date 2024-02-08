b1e647b92c753eac6cba7211ad4171f008dda4a3
Neil Griffin <140093634+GriffoSligo@users.noreply.github.com>
2024-02-08 15:13:57 +0000

    Intune Proactive Remediations

---
+++ Proactive-Remediations/Detection-Last_Reboot-7days.ps1
@@ -0,0 +1,8 @@
+﻿$Uptime= get-computerinfo | Select-Object OSUptime 
+if ($Uptime.OsUptime.Days -ge 7){
+    Write-Output "Device has not rebooted on $($Uptime.OsUptime.Days) days, notify user to reboot"
+    Exit 1
+}else {
+    Write-Output "Device HAS rebooted $($Uptime.OsUptime.Days) days ago, all good"
+    Exit 0
+}
